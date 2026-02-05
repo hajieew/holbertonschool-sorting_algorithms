ba5447d661e643610c54745ff75ccc3fd3fa7b08
hajieew <hajieew@gmail.com>
2026-02-05 13:23:16 +0000

    python

--- sort.h
+++ sort.h
@@ -4,6 +4,13 @@
 #include <stddef.h>
 #include <stdio.h>
 
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
